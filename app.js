dd02d2ef21ecc6ca6e2478a1fa7a912653cf6524
Max Olson <maxprogram@gmail.com>
2013-07-22 21:17:07 -0600

    Start express app & routes

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    gcj = require('grand-central-junction'),
+    app = express();
+
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(require('less-middleware')({ src: __dirname + '/assets' }));
+    app.use(gce.pipeline());
+    app.use(express.static(path.join(__dirname, 'assets')));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler());
+});
+
+gcj.route(app);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
