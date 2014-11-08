efa7a546094de6f84122415edf75b3a5120755cf
Max <maxprogram@gmail.com>
2014-11-08 10:54:43 -0800

    Add new Shape methods for update, relations

--- models/Shape.js
+++ models/Shape.js
@@ -8,8 +8,8 @@ var _ = require('lodash'),
 
 var Shape = module.exports = pg.model("shapes", {
     schema: {
-        type_id: Number,
-        periods: [Number],
+        type_id: { type: Number, allowNull: false },
+        periods: { type: [Number], allowNull: false },
         start_day:   { type: Number, default: 1 },
         start_month: { type: Number, default: 1 },
         start_year:  { type: Number },
@@ -60,6 +60,29 @@ var cleanShapeData = Shape.cleanShapeData = function(data) {
     return newData;
 };
 
+// Validates shape relation object
+function validateRelation(rel) {
+    var keys = ['type', 'id', 'role', 'sequence'];
+    var types = ['Node', 'Way', 'Shape'];
+    var roles = ['outer', 'inner', 'point', 'center', 'line'];
+
+    rel = _.pick(rel, keys);
+
+    if (!_.isString(rel.id) || _.isNumber(rel.id)) rel.invalid = true;
+
+    rel.type = rel.type.charAt(0).toUpperCase() + rel.type.slice(1);
+    if (!_.contains(types, rel.type)) rel.invalid = true;
+
+    rel.role = rel.role.toLowerCase();
+    if (!_.contains(roles, rel.role)) rel.invalid = true;
+
+    if (rel.sequence) {
+        rel.sequence = parseFloat(rel.sequence);
+        if (isNaN(rel.sequence)) rel.invalid = true;
+    }
+    return rel;
+}
+
 // Gets a single shape with associated nodes/ways
 Shape.get = function(id) {
     var shape = {};
@@ -70,7 +93,7 @@ Shape.get = function(id) {
         Shape.getRelations(id)
     ]).spread(function(shapes, relations) {
         return {
-            properties: shapes[0].toJSON(),
+            properties: _.isEmpty(shapes) ? null : shapes[0].toJSON(),
             objects: relations.map(function(rel) {
                 return {
                     type: rel.relation_type,
@@ -92,9 +115,8 @@ Shape.inChangeset = function(id) {
     }));
 };
 
-Shape.getRelations = function(id, callback) {
-    if (Array.isArray(id)) id = id.join(",");
-    return Shape.Relation.where('shape_id IN ('+id+')').order('sequence_id', callback);
+Shape.getRelations = function(ids) {
+    return Shape.Relation.where({ shape_id: ids }).order('sequence_id');
 };
 
 /**
@@ -147,7 +169,7 @@ Shape.getData = function(options) {
  *   or   {number}      .year      Year shape is in
  *        {(num|num[])} [.type]    Type ID(s)
  *        {number[]}    [.bbox]    Bounding box (west, south, east, north)
- * @returns {array} Array of objects, each with shape data
+ * @returns {array} Array of objects, each with shape nodes
  */
 Shape.getNodes = function(options) {
     var queue = pg.queue(),
@@ -187,7 +209,7 @@ Shape.getNodes = function(options) {
     order = "ORDER BY shape, seq1, seq2 ";
     where = "WHERE shape_relations.shape_id ";
 
-    if (_.isNumber(shapes)) shapes = [shapes];
+    if (_.isNumber(shapes) || _.isString(shapes)) shapes = [shapes];
 
     if (Array.isArray(shapes)) {
         where += "IN (:shapes) ";
@@ -222,38 +244,88 @@ Shape.getNodes = function(options) {
 };
 
 Shape.create = function(data) {
+    data = _.clone(data);
     data = cleanShapeData(data);
     return this.returning("id").insert(data);
 };
 
-// Connects a shape with current nodes/ways/shapes
-// (nodes must be created first)
+Shape._update = Shape.update;
+Shape.update = function(id, data) {
+    data = _.clone(data);
+    data.data = null;
+
+    // Clean & validate data
+    var newData = cleanShapeData(data);
+    var validate = Shape.new(newData).validate();
+    data = _.pick(newData, _.keys(data));
+    validate = _.pick(validate, _.keys(data));
+    if (!_.isEmpty(validate)) return err.reject(validate, 'invalid');
+
+    // Prep hstore update
+    if (data.data) data.data = [['data || ('+data.data[0][0]+')']]
+
+    return Shape._update(id, data);
+};
+
+/**
+ * Connects a shape with existing nodes/ways
+ * @param {number} shapeId
+ * @param {number[]} relations Array of relation objects
+ *                  .id, .type, .role, .sequence
+ */
 Shape.connect = function(shapeId, relations) {
-    var rels = [],
-        i = 0;
+    var i = 0;
 
     if (relations.length === 0)
-        return err.reject('no nodes, ways, or shapes','connecting shape');
+        return err.reject('no nodes or ways','connecting shape');
+    relations = relations.map(validateRelation);
+    var invalid = _.find(relations, { invalid: true });
+    if (!_.isEmpty(invalid))
+        return err.reject('relations invalid: ' + JSON.stringify(invalid),'connecting shape');
 
-    relations.forEach(function(rel) {
-        rels.push({
+    relations = relations.map(function(rel) {
+        return {
             shape_id: shapeId,
             relation_type: rel.type,
             relation_id: rel.id,
             relation_role: rel.role || ' ',
             sequence_id: rel.sequence || i++
-        });
+        };
     });
 
-    return Shape.Relation.insert(rels).then(function(res) {
-        return shapeId;
-    });
+    return Shape.Relation.insert(relations).thenResolve(shapeId);
 };
 
+// Creates shape & relations
 Shape.finish = function(data, relations) {
-    return Shape.create(data).then(function(shapes) {
-        var id = shapes[0].id;
-        return Shape.connect(id, relations);
+    return Shape.create(data).thenOne(function(shape) {
+        return Shape.connect(shape.id, relations);
     });
 };
+
+/**
+ * Remove relations from shape
+ * @param {number} shapeId
+ * @param {number[]} seqIds Array of sequence IDs to remove
+ */
+Shape.addQueryMethod('removeRelations', function(shapeId, seqIds) {
+    if (!_.isArray(seqIds)) seqIds = [seqIds];
+
+    var sequence = _.uniqueId('shape_seq_'),
+        queue = pg.queue()
+        .add(Shape.Relation.where({ shape_id: shapeId, sequence_id: seqIds }).remove());
+
+    // Re-numbers sequence without losing order
+    queue.add('CREATE SEQUENCE ' + sequence)
+      .add("UPDATE shape_relations SET sequence_id = newseq FROM (" +
+        "SELECT sequence_id, (nextval(:seq) - 1) AS newseq FROM " +
+            "(SELECT sequence_id FROM shape_relations WHERE shape_id = :shape ORDER BY sequence_id) AS sub" +
+        ") AS new_table WHERE " +
+        "shape_relations.shape_id = :shape AND " +
+        "shape_relations.sequence_id = new_table.sequence_id",
+        { shape: shapeId, seq: sequence })
+      .add('DROP SEQUENCE ' + sequence);
+
+    return queue;
+});
 
